f284d6a8471ccb43419c774fe7a6808f2126f78c
addie293 <40389482+addie293@users.noreply.github.com>
2021-04-07 10:11:30 +0200

    This is a simple application that involves representing five friends in a 2 km radius around the Empire State Building on a Google Map using Map JavaScript API

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function initMap(){
     // Map option
 
     var options = {
-        center: {lat: 40.7128 , lng:-74.0060 },
-        zoom: 10
+        center: {lat: 40.748817 , lng:-73.985428 },
+        zoom: 16
     }
 
     //New Map
@@ -17,60 +17,67 @@ function initMap(){
         addMarker({location:event.latLng});
     })
 
+    let MarkerArray = [ {location:{lat: 40.748817, lng: -73.985428},  
+        content: `<h2>Empire State Building</h2>`,
+    type: "location"},
 
+        {location:{lat: 40.7678, lng: -73.9718},
+        content: `<h2>Oluchi Ibenema</h2>`,
+    type: "friend"},
 
-    //Marker
-/*
-    const marker = new google.maps.Marker({
-    position:{lat: 37.9922, lng: -1.1307},
-    map:map,
-    icon:"https://img.icons8.com/nolan/2x/marker.png"
-    });
-    //InfoWindow
-    const detailWindow = new google.maps.InfoWindow({
-        content: `<h2>Murcia City</h2>`
-    });
-    marker.addListener("mouseover", () =>{
-        detailWindow.open(map, marker);
-    })
-    */
+        {location:{lat: 40.739388, lng: -74.009933},
+        content: `<h2>Allan Bernon</h2>`,
+        type: "friend"},
 
-    //Add Markers to Array
+        {location:{lat: 40.7264, lng: - 73.9818},
+        content: `<h2>Levi Martins</h2>`,
+        type: "friend"},
+        
+        {location:{lat: 40.7253, lng: - 73.9903},
+        content: `<h2>Aditya Das</h2>`,
+        type: "friend"},
 
-    let MarkerArray = [ {location:{lat: 40.748817, lng: -73.985428}, 
-        imageIcon: "https://img.icons8.com/nolan/2x/marker.png", 
-        content: `<h2>Empire State Building</h2>`},
-
-        {location:{lat: 40.748817, lng: -73.985428}},
-
-        {location:{lat: 40.7196, lng: -74.0066},content: `<h2>Oluchi Ibenema</h2>`}
-
+        {location:{lat: 40.7280, lng: - 74.0002},
+        content: `<h2>Rediet Tedesse</h2>`,
+        type: "friend"},
 
 
     ]
 
+    const svgMarker = {
+        scale: 0,
+        anchor: new google.maps.Point(15, 30),
+      };
+
     // loop through marker
     for (let i = 0; i < MarkerArray.length; i++){
-        addMarker(MarkerArray[i]);
+        addMarker(MarkerArray[i], i);
 
     }
 
     // Add Marker
 
-    function addMarker(property){
+    function addMarker(property, index){
 
         const marker = new google.maps.Marker({
             position:property.location,
+            scaledSize: new google.maps.Size(1, 1),
             map:map,
+            icon: {
+                url: property.type == "friend" ? 'friend'+index+'.jpeg' : '',
+                scaledSize: new google.maps.Size(50, 50)
+            }
             //icon: property.imageIcon
             });
 
             // Check for custom Icon
 
-            if(property.imageIcon){
-                // set image icon
-                marker.setIcon(property.imageIcon)
-            }
+            // if(property.type == "friend"){
+            //     console.log('friend'+index+'.jpeg')
+            //     // set image icon
+            //     //console.log('hello')
+            //     marker.setIcon('friend'+index+'.jpeg')
+            // }
 
             if(property.content){
 
